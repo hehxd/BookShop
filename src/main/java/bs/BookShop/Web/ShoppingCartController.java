93787b65f0f7fc4741ed907dc60dff69a7fdb1ec
TeodoraKrstevska201137 <teodora.krstevska@students.finki.ukim.mk>
2024-05-20 00:38:53 +0200

    Update on ShoppingCartController

--- src/main/java/bs/BookShop/Web/ShoppingCartController.java
+++ src/main/java/bs/BookShop/Web/ShoppingCartController.java
@@ -5,6 +5,7 @@ import bs.BookShop.Model.ShoppingCart;
 import bs.BookShop.Model.dto.CartItemDto;
 import bs.BookShop.Service.BookService;
 import bs.BookShop.Service.ShoppingCartService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,33 +23,25 @@ public class ShoppingCartController {
     }
 
 
-    @GetMapping("/add/{id}")
-    public void addToCart(@RequestBody CartItemDto cartItemDto) {
-        shoppingCartService.addToCart(cartItemDto);
-    }
-
-    @GetMapping("/remove/{id}")
-    public void removeFromCart(@PathVariable Long id) {
-        Book book = bookService.findById(id).get();
-        shoppingCartService.removeFromCart(book);
-    }
-
-
     @GetMapping()
     public ShoppingCart viewCart() {
        return shoppingCartService.getShoppingCart();
     }
 
-    @PostMapping("/add/{id}")
-    public void addToCartPost(@RequestBody CartItemDto cartItemDto) {
+    @PostMapping("/add")
+    public ResponseEntity<String> addToCartPost(@RequestBody CartItemDto cartItemDto) {
         shoppingCartService.addToCart(cartItemDto);
-
+        return ResponseEntity.ok("Item added to cart");
     }
 
     @PostMapping("/remove/{id}")
     public void removeFromCartPost(@PathVariable Long id) {
         Book book = bookService.findById(id).get();
         shoppingCartService.removeFromCart(book);
+    }
+    @PostMapping("/clear")
+    public void clearCart(){
+        shoppingCartService.clearCart();
     }
 
 }
